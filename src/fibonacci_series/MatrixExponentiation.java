c9adb36357db566780627a35be06406658443525
Berkay Ataman <brkyataman@gmail.com>
2016-07-25 03:54:05 +0300

    Update MatrixExponentiation.java

--- src/fibonacci_series/MatrixExponentiation.java
+++ src/fibonacci_series/MatrixExponentiation.java
@@ -5,21 +5,22 @@ public class MatrixExponentiation {
 	int[][] unitMatrix = {{1,1},{1,0}};
 	
 	int MatrixFib(int n){
-		pow(n-1);
+		pow(n-1);//M[0][0] gives us n+1 so we are calculating 'n-1'th fib
 		return M[0][0];
 	}
 	//Calculating 'n' by squares.
 	void pow(int n){
 		if(n > 1){
-			pow(n/2);
-			M = MatrixMultiply(M,M);
+			pow(n/2); //Calling pow recursively
+			M = MatrixMultiply(M,M); //Climbing up by squares
 		}
 		
 		if(n % 2 == 1){//n is odd
-			M = MatrixMultiply(M,unitMatrix);
+			M = MatrixMultiply(M,unitMatrix); //If n is odd than one unit matrix is missed so we are multiplying
 		}
 	}
 	
+	//Multiplying two matrixes
 	int[][] MatrixMultiply(int[][] A, int[][] B){
 		int[][] result = {{0,0},{0,0}};
 		
